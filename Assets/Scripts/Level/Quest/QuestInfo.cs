e7d37eb0ecda259c11a24e9d9f5f7d65e1b77c5f
최석환 <seokhwan98@naver.com>
2025-04-18 18:07:24 +0900

    Revert "Feat/level"

--- Assets/Scripts/Level/Quest/QuestInfo.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class QuestInfo
-{
-    public const string GameClearQuestID = "Quest_End";
-    public const string GoalOpenQuestID = "Quest_Open";
-}
--- Assets/Scripts/StageSelect/StageInfo.cs
+++
@@ -1,5 +0,0 @@
-public static class StageInfo
-{
-    public static LevelData SelectedLevel;
-    
-}
